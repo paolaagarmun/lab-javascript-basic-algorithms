270a2c4491b4ef86bcc53e75938a00a11bf1e604
paolaagarmun <paola.a.garciamunoz@gmail.com>
2022-01-10 09:59:02 -0500

    Iteration 1 completed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 // Iteration 1: Names and Input
-// 
+//1.1
+let hacker1  = "John";
+//1.2
+console.log(`The driver's name is ${hacker1}`);
+//1.3
+let hacker2 = "Mary";
+//1.4
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+//2.1
 
 
 // Iteration 3: Loops
